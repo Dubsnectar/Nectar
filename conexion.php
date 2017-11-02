b45e3e517c4139f14db0c314bf5cfc5f3aa1eb6c
Dubsnectar <32078693+Dubsnectar@users.noreply.github.com>
2017-11-01 18:24:39 -0600

    Delete conexion.php

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "usbw", "farmacia");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
-
-?>
-
